896d269137b530afc5b8675a70bd79f100d286a0
Kamar Mack <kamarmack.home@gmail.com>
2023-12-25 09:36:28 +0000

    Append v0 to getCustomToken function

--- src/features/authentication/api/getCustomToken.tsx
+++ src/features/authentication/api/getCustomToken.tsx
@@ -2,7 +2,7 @@ import { default as ky } from 'ky-universal';
 
 export const getCustomToken = async ({ id_token }: { id_token: string }) => {
 	const data = await ky
-		.post('auth/custom-token', {
+		.post('v0/auth/custom-token', {
 			json: { id_token },
 			prefixUrl: process.env.NEXT_PUBLIC_REST_API_BASE_URL,
 		})
