23ae40a4c326978bb37004798fd087b1f38c0552
Kamar Mack <kamarmack.home@gmail.com>
2023-12-31 11:57:23 +0000

    Add getCachedFirebaseUserJwt function; Add getAuthenticatedGeneralizedKyInstance function

---
+++ src/features/authentication/utils/getCachedFirebaseUserJwt.ts
@@ -0,0 +1,35 @@
+import { User as FirebaseUser } from 'firebase/auth';
+import { isDomAvailable } from '../../../utils/isDomAvailable';
+
+export const getCachedFirebaseUserJwt = async (
+	firebaseUser: FirebaseUser,
+	options: { forceRefresh?: boolean } = {},
+) => {
+	try {
+		if (!isDomAvailable()) {
+			console.error('DOM not available');
+			return null;
+		}
+
+		const cachedToken = localStorage.getItem('firebase_auth_jwt');
+		const forceRefresh = options?.forceRefresh === true;
+		const token =
+			forceRefresh || !cachedToken
+				? await firebaseUser.getIdToken(forceRefresh)
+				: cachedToken;
+
+		if (!token) {
+			console.error('No token found');
+			return null;
+		}
+
+		if (!cachedToken) {
+			localStorage.setItem('firebase_auth_jwt', token);
+		}
+
+		return token;
+	} catch (err) {
+		console.error('Error getting firebase auth token for ky instance', err);
+		return null;
+	}
+};
--- src/lib/ky.ts
+++ src/lib/ky.ts
@@ -1,4 +1,54 @@
 import { default as ky } from 'ky-universal';
+import { User as FirebaseUser } from 'firebase/auth';
 import { generalizedKyConfig } from '../config/kyConfig';
+import { getCachedFirebaseUserJwt } from '../features/authentication';
 
 export const generalizedKyInstance = ky.create(generalizedKyConfig);
+export const getAuthenticatedGeneralizedKyInstance = (
+	firebaseUser: FirebaseUser,
+	defaultKyInstance: typeof ky = generalizedKyInstance,
+) =>
+	defaultKyInstance.extend({
+		hooks: {
+			beforeRequest: [
+				async (request) => {
+					// Get the Firebase Auth ID token
+					const token = await getCachedFirebaseUserJwt(firebaseUser);
+
+					// If no token found, throw an error
+					if (!token) throw new Error('No token found');
+
+					// Set the Authorization header
+					request.headers.set('Authorization', `Bearer ${token}`);
+				},
+			],
+			afterResponse: [
+				async (request, options, response) => {
+					if (response.status === 403) {
+						// Check whether a retry has already been attempted
+						if (request.headers.get('X-Retry-Attempt')) {
+							return response;
+						}
+
+						// Get a fresh token
+						const token = await getCachedFirebaseUserJwt(firebaseUser, {
+							forceRefresh: true,
+						});
+
+						// If no token found, throw an error
+						if (!token) throw new Error('No token found');
+
+						// Set the Authorization header
+						request.headers.set('Authorization', `Bearer ${token}`);
+
+						// Indicate that this is a retry
+						request.headers.set('X-Retry-Attempt', '1');
+
+						// Retry the request
+						return defaultKyInstance(request, options);
+					}
+					return response;
+				},
+			],
+		},
+	});
