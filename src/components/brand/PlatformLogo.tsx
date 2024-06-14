d199516cdc7faf2126428a2756e3e83af9a163c4
Kamar <kamarmack.home@gmail.com>
2024-06-14 23:06:08 +0000

    Add a useIsMounted hook; Leverage useIsMounted in <PlatformLogo>

--- src/components/brand/PlatformLogo.tsx
+++ src/components/brand/PlatformLogo.tsx
@@ -5,6 +5,20 @@ import {
 	BaseComponentTheme,
 } from '../../types/BaseComponentTypes';
 import { default as cn } from '../../lib/cn';
+import { useIsMounted } from '../../hooks/useIsMounted';
+
+type PlatformLogoFallbackProps = {
+	className: string;
+	size: string;
+};
+export const PlatformLogoFallback: React.FC<PlatformLogoFallbackProps> = ({
+	className,
+	size,
+}) => (
+	<div
+		className={cn(size, className, 'animate-pulse bg-gray-300 rounded-md')}
+	/>
+);
 
 const PLATFORM_LOGO_SIZE = {
 	sm: 'w-16',
@@ -34,12 +48,32 @@ export const PlatformLogo: React.FC<PlatformLogoProps> = ({
 	width,
 }) => {
 	const { resolvedTheme } = useTheme();
+	const isMounted = useIsMounted();
+
+	if (!isMounted) {
+		return (
+			<PlatformLogoFallback
+				className={className}
+				size={PLATFORM_LOGO_SIZE[size]}
+			/>
+		);
+	}
+
 	const isDarkMode = logoTheme === 'dark' || resolvedTheme === 'dark';
 	const logoSize = customLogoSizeClassName || PLATFORM_LOGO_SIZE[size];
 	const src =
 		srcMap[
 			logoTheme === 'auto' ? (resolvedTheme as 'dark' | 'light') : logoTheme
 		];
+
+	if (typeof src !== 'string' || !src) {
+		return (
+			<PlatformLogoFallback
+				className={className}
+				size={PLATFORM_LOGO_SIZE[size]}
+			/>
+		);
+	}
 
 	if (isDarkMode) {
 		return (
