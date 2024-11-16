08a78def7bbfd1bcea0287a22eaef9212dafe55a
Kamar <kamarmack.home@gmail.com>
2024-11-16 21:10:19 +0000

    Select the stripe publishable key based on DEPLOYMENT_ENV

--- src/config/stripeConfig.ts
+++ src/config/stripeConfig.ts
@@ -1,7 +1,19 @@
-if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+import { DEPLOYMENT_ENV } from './deploymentEnv';
+
+const productionStripePublishableKey =
+	process.env.NEXT_PUBLIC_PRODUCTION_STRIPE_PUBLISHABLE_KEY;
+const stagingStripePublishableKey =
+	process.env.NEXT_PUBLIC_STAGING_STRIPE_PUBLISHABLE_KEY;
+
+const stripePublishableKey = {
+	production: productionStripePublishableKey,
+	staging: stagingStripePublishableKey,
+}[DEPLOYMENT_ENV];
+
+if (!stripePublishableKey) {
 	throw new Error('Missing Stripe publishable key');
 }
 
 export const STRIPE_CONFIG = {
-	publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+	publishableKey: stripePublishableKey,
 };
