082903bec7b7ed802ca0ba0fb5e586d194d3886c
Kamar <kamarmack.home@gmail.com>
2024-11-16 20:45:47 +0000

    Fix enum bug in ed24f75

--- src/config/nodeEnv.ts
+++ src/config/nodeEnv.ts
@@ -5,9 +5,9 @@ export const NodeEnvironmentEnum = getEnum([
 	'production',
 	'test',
 ]);
-export type NodeEnvironment = EnumMember<typeof NodeEnvironmentEnum.obj>;
+export type NodeEnvironment = EnumMember<typeof NodeEnvironmentEnum>;
 
-export const NODE_ENV: 'development' | 'production' | 'test' =
+export const NODE_ENV: NodeEnvironment =
 	process.env.JEST_WORKER_ID === undefined
 		? (process.env.NODE_ENV as NodeEnvironment | undefined) || 'development'
 		: 'test';
