ed24f75ebfdf17d9edf308ba213c200d7a6ecce9
Kamar <kamarmack.home@gmail.com>
2024-11-16 20:39:57 +0000

    Create an enum for Node environments

--- src/config/nodeEnv.ts
+++ src/config/nodeEnv.ts
@@ -1,4 +1,11 @@
-export type NodeEnvironment = 'development' | 'production' | 'test';
+import { getEnum, EnumMember } from 'ergonomic';
+
+export const NodeEnvironmentEnum = getEnum([
+	'development',
+	'production',
+	'test',
+]);
+export type NodeEnvironment = EnumMember<typeof NodeEnvironmentEnum.obj>;
 
 export const NODE_ENV: 'development' | 'production' | 'test' =
 	process.env.JEST_WORKER_ID === undefined
