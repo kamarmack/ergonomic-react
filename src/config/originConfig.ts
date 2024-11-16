0a2f1cb3cf6d5d4f8c7bbe592b6a3b2bbdcddac5
Kamar <kamarmack.home@gmail.com>
2024-11-16 21:22:15 +0000

    Throw an error if the site URL is not set as an environment var

--- src/config/originConfig.ts
+++ src/config/originConfig.ts
@@ -2,7 +2,13 @@ import { DEPLOYMENT_ENV } from './deploymentEnv';
 
 const productionSiteOrigin = process.env.NEXT_PUBLIC_PRODUCTION_SITE_URL;
 const stagingSiteOrigin = process.env.NEXT_PUBLIC_STAGING_SITE_URL;
-export const SITE_ORIGIN = {
+export const siteOrigin = {
 	production: productionSiteOrigin,
 	staging: stagingSiteOrigin,
 }[DEPLOYMENT_ENV];
+
+if (!siteOrigin) {
+	throw new Error('Missing site origin');
+}
+
+export const SITE_ORIGIN = siteOrigin;
