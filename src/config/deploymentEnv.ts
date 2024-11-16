d433197acaa3ea4309043fd913c6d5f4a4761a0a
Kamar <kamarmack.home@gmail.com>
2024-11-16 21:06:49 +0000

    Add documentation to DEPLOYMENT_ENV

--- src/config/deploymentEnv.ts
+++ src/config/deploymentEnv.ts
@@ -17,4 +17,11 @@ if (
 	);
 }
 
+/**
+ * The deployment environment of the application.
+ *
+ * This value is used to determine configuration settings for the application, for example, Firebase configuration settings.
+ *
+ * See https://firebase.google.com/docs/projects/dev-workflows/overview-environments for an overview of how to manage Firebase configuration settings for pre-production and production environments.
+ */
 export const DEPLOYMENT_ENV = process.env.NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT;
