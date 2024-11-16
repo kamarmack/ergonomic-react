fb580c832f2654b578994f50fbe819fa433fff9e
Kamar <kamarmack.home@gmail.com>
2024-11-16 21:00:48 +0000

    Select the base URL for API requests based on DEPLOYMENT_ENV

--- src/config/kyConfig.ts
+++ src/config/kyConfig.ts
@@ -1,11 +1,21 @@
 import { Options } from 'ky-universal';
+import { DEPLOYMENT_ENV } from './deploymentEnv';
+
+const productionRestAPIBaseURL =
+	process.env.NEXT_PUBLIC_PRODUCTION_REST_API_BASE_URL;
+const stagingRestAPIBaseURL = process.env.NEXT_PUBLIC_STAGING_REST_API_BASE_URL;
+
+export const REST_API_PREFIX_URL = {
+	production: productionRestAPIBaseURL,
+	staging: stagingRestAPIBaseURL,
+}[DEPLOYMENT_ENV];
 
 export const GENERALIZED_KY_CONFIG: Options = {
 	headers: {
 		'Content-Type': 'application/json',
 		Accept: 'application/json',
 	},
-	prefixUrl: process.env.NEXT_PUBLIC_REST_API_BASE_URL,
+	prefixUrl: REST_API_PREFIX_URL,
 	throwHttpErrors: true,
 	timeout: 45 * 1000, // Timeout after 45 seconds
 };
