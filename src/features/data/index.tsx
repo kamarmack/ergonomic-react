fdb622bd1b7cb5127f9621caa94c811e6e412607
Kamar <kamarmack.home@gmail.com>
2024-11-18 02:22:10 +0000

    Add additional exports from data feature entry point

--- src/features/data/index.tsx
+++ src/features/data/index.tsx
@@ -1,1 +1,5 @@
 export * from './components/GeneralizedForm';
+export * from './components/GeneralizedTableCell';
+export * from './types/FirestoreQueryTypes';
+export * from './types/GeneralizedFormProps';
+export * from './types/GeneralizedTableCellProps';
