620da9c312bde0073d38cbdfcd144e842d510fba
Kamar <kamarmack.home@gmail.com>
2024-12-18 18:11:52 +0000

    Add DateTimeCell component for enhanced date-time handling in tables

---
+++ src/features/data/components/cells/DateTimeCell.tsx
@@ -0,0 +1,17 @@
+import { DateTime } from 'luxon';
+import { YupHelpers } from 'ergonomic';
+import { GeneralizedTableCellProps } from '../../types/GeneralizedTableCellProps';
+import { DefaultCell } from './DefaultCell';
+
+export const DateTimeCell = (props: GeneralizedTableCellProps): JSX.Element => (
+	<DefaultCell
+		{...props}
+		formatValue={(value: unknown): string => {
+			const date = value as string;
+			return DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_FULL);
+		}}
+		isValid={(value: unknown): boolean =>
+			YupHelpers.dateTime().isValidSync(value)
+		}
+	/>
+);
